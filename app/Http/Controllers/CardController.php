162f7dddba4ea61a139e3bd9119ae198914f6a9e
Ranieli <ranielisilveira@hotmail.com>
2021-06-10 12:30:30 -0300

    feat: cards update

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -38,13 +38,6 @@ class CardController extends Controller
 
     public function store(Request $request)
     {
-        /*
-        always required
-        name
-        class
-        type
-        set
-        */
         try {
             $validator = Validator::make($request->all(), [
                 'name' => 'required|unique:cards',
@@ -86,7 +79,47 @@ class CardController extends Controller
 
     public function update(Request $request, $id)
     {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|unique:cards,name,' . $id . ',id',
+                'card_class_id' => 'required|exists:card_classes,id',
+                'card_type_id' => 'required|exists:card_types,id',
+                'card_set_id' => 'required|exists:card_sets,id',
+            ], [
+                'name.required' => 'Você deve preencher o nome da carta.',
+                'name.unique' => 'Já existe uma carta com este nome, favor especificar um nome diferente.',
+
+                'card_class_id.required' => 'Você deve selecionar a classe da carta.',
+                'card_class_id.exists' => 'Você deve selecionar uma classe válida para a carta.',
+
+                'card_type_id.required' => 'Você deve selecionar o tipo da carta.',
+                'card_type_id.exists' => 'Você deve selecionar um tipo válido para a carta.',
+
+                'card_set_id.required' => 'Você deve selecionar o set da carta.',
+                'card_set_id.exists' => 'Você deve selecionar um set válido para a carta.',
+            ]);
+
+            if ($validator->fails()) {
+                throw new Exception($validator->errors()->first());
+            }
+
+            $card = Card::findOrFail($id);
+
+            $card->update($request->all());
+
+            return response([
+                'message' => 'Carta atualizada com sucesso!',
+                'card' => Card::with([
+                    'cardType',
+                    'cardSet',
+                    'cardClass',
+                ])->find($id)
+            ], Response::HTTP_OK);
+        } catch (Exception $exception) {
+            return response(['message' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
     }
+
 
     public function show($id)
     {

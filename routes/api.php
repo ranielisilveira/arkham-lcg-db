e73942acae4eb576d87fe769a4e9e290c292b68a
Ranieli <ranielisilveira@hotmail.com>
2021-06-18 11:35:22 -0300

    feat: cors fix

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,8 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Auth\Events\Login;
-use Laravel\Lumen\Routing\Router;
+use Dusterio\LumenPassport\LumenPassport;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +20,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+
 $router->post('/login', 'AuthController@login');
 $router->post('/register', 'RegisterController@store');
 $router->get('/confirm-account/{token}', 'RegisterController@confirmAccount');
@@ -28,15 +28,22 @@ $router->get('/confirm-account/{token}', 'RegisterController@confirmAccount');
 $router->post('/password-recovery', 'PasswordRecoveryController@sendPasswordRecovery');
 $router->post('/password-change', 'PasswordRecoveryController@changePassword');
 
-$router->get('/cards', 'CardController@index'); //lista todas as cartas/itens
+
+$router->get('/cards', 'CardController@index'); // lista todas as cartas/items
 $router->get('/cards/{id}', 'CardController@show'); // listar somente um card com o id enviado
 
 $router->group(['middleware' => 'auth:api'], function ($router) {
+    $router->get('/me', function (Request $request) {
+        return [
+            'user' => $request->user()
+        ];
+    });
     $router->post('/logout', 'AuthController@logout');
 
     $router->group(['prefix' => 'cards'], function ($router) {
-        $router->post('/', 'CardController@store'); //criar um card
-        $router->put('/{id}', 'CardController@update'); //atualizar um card
+        $router->post('/', 'CardController@store'); // criar um card
+        $router->put('/{id}', 'CardController@update'); // atualizar um card
+
         $router->delete('/{id}', 'CardController@delete');
     });
 });
